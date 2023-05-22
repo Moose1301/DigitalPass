b171ce757e48ed2525d15e0b809631a65418a520
Moose1301 <40875577+Moose1301@users.noreply.github.com>
2023-05-22 17:02:13 +0000

    Create base profile

---
+++ frontend/pages/profile/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="content" class="with-sidebar">
+    <div class="container">Hello {{ $auth.user.name_first }} </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    middleware: ['auth']
+
+}
+</script>
