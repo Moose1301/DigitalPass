5ceb341b90748b5e868624cb9671d7a04a1e2b5f
Moose1301 <40875577+Moose1301@users.noreply.github.com>
2023-05-24 16:52:41 +0000

    Start on Actual Pass Page

--- frontend/pages/pass/_id/index.vue
+++ frontend/pages/pass/_id/index.vue
@@ -1,19 +1,40 @@
 <template>
-  <div id="content" class="with-sidebar">
-    <div class="container">Pass ID: {{ $route.params.id }}</div>
+  <div class="text-center">
+    <div class="bg-black rounded-lg mx-auto mt-8 max-w-sm h-96">
+      <div>
+        Issuer: {{ pass.issuedBy }}
+      </div>
+      <div>
+        At: {{ format_date(pass.issuedAt) }}
+      </div>
+      <div>
+        From: {{ pass.roomFrom }}
+      </div>
+      <div>
+        To: {{ pass.roomTo }}
+      </div>
+      <img class="w-16 md:w-32 lg:w-48" src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&choe=UTF-8&chl=%PAGE URL%">
+    </div>
+
   </div>
+      
+
 </template>
 
 <script>
 import { Pass } from "~/types/Pass";
-
+import moment from "moment";
 export default {
   
   middleware: ['auth'],
   data: () => ({
       pass: Pass
   }),
-
+  methods: {
+    format_date(value) {
+      return moment(String(value)).format("MM/DD HH:mm")
+    }
+  },
   async fetch() {
     const pass = await this.$axios
       .get("/pass/" + this.$route.params.id)
